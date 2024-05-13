5cedb044e6cb77738d7e6999f752feed84b09489
krylovolegv <krylovolegv@yandex.ru>
2024-05-13 13:17:24 +0200

    post-request_to_server

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
-img = "https://sun9-21.userapi.com/impg/S99EVTZNi9e9CfQLGoZFYG5DlFbyfugWJrUEGw/77y2k9wXdEo.jpg?size=860x484&quality=95&sign=dd48352b65e741350ae537000676cac9&type=album"
+url = "https://jsonplaceholder.typicode.com/posts"
 
-response = requests.get(img)
+data = {
+    "title" : "тестовый post запрос",
+    "body" : "тестовый контент post запроса",
+    "userId" : 2
+}
 
-with open("test.jpg", "wb") as file:
-    file.write(response.content)
+response = requests.post(url, data=data)
+
+print(response.status_code)
+print (f"ответ - {response.json()}")
